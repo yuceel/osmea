f662359d070d7796d1a709378b47eaa368606367
melihalkbk <melihalkbk@gmail.com>
2025-07-09 18:27:17 +0300

    refactor: improve code formatting and consistency in SectionContainerWidget and HeaderSection

--- packages/components/example_storybook/lib/storybook_test/components/cards_test_modular/widgets/section_container_widget.dart
+++ packages/components/example_storybook/lib/storybook_test/components/cards_test_modular/widgets/section_container_widget.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:osmea_components/osmea_components.dart';
 
 /// 📦 **Section Container Widget**
-/// 
+///
 /// Reusable container for organizing card showcases into sections
 class SectionContainerWidget extends StatelessWidget {
   final String title;
@@ -41,7 +41,7 @@ class SectionContainerWidget extends StatelessWidget {
         border: Border.all(color: borderColor, width: 1),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.05),
+            color: Colors.black.withValues(alpha: 0.05),
             blurRadius: 8,
             offset: const Offset(0, 2),
           ),
@@ -84,4 +84,4 @@ class SectionContainerWidget extends StatelessWidget {
       ),
     );
   }
-} 
+}
