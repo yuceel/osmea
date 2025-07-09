abeb62eb89c2130518861070008e4ce644cd9825
melihalkbk <melihalkbk@gmail.com>
2025-07-09 18:27:23 +0300

    refactor: improve code formatting and consistency in SectionContainerWidget for checkbox and chips showcases

--- packages/components/example_storybook/lib/storybook_test/components/chips_test_modular/widgets/section_container_widget.dart
+++ packages/components/example_storybook/lib/storybook_test/components/chips_test_modular/widgets/section_container_widget.dart
@@ -41,7 +41,7 @@ class SectionContainerWidget extends StatelessWidget {
         border: Border.all(color: borderColor, width: 1),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.05),
+            color: Colors.black.withValues(alpha: 0.05),
             blurRadius: 8,
             offset: const Offset(0, 2),
           ),
@@ -84,4 +84,4 @@ class SectionContainerWidget extends StatelessWidget {
       ),
     );
   }
-} 
+}
