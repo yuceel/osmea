b6abf02816f58a7a18961d6078ae76ab2b0697f6
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2025-07-15 20:58:53 +0300

    refactor: improve code formatting and readability in SplashView by adjusting line breaks and indentation

--- packages/core/example/lib/views/view_splash/splash_view.dart
+++ packages/core/example/lib/views/view_splash/splash_view.dart
@@ -19,26 +19,31 @@ class SplashView extends MasterView<SplashViewModel, SplashEvent, SplashState> {
     try {
       debugPrint('Checking if this is the first launch');
       // Check if the first launch timestamp exists
-      final firstLaunchTimestamp = await localStorageHelper.getItem('FirstLaunchTimestamp');
-      
+      final firstLaunchTimestamp =
+          await localStorageHelper.getItem('FirstLaunchTimestamp');
+
       if (firstLaunchTimestamp == null) {
         // This is the first launch
         debugPrint('Setting item in local storage for the first launch');
         localStorageHelper.setItem('SplashViewFirst', true);
-        localStorageHelper.setEncryptedItem('SplashViewFirstEncrypted', 'I am a secret');
-        
+        localStorageHelper.setEncryptedItem(
+            'SplashViewFirstEncrypted', 'I am a secret');
+
         // Store the current date and time as the first launch timestamp
-        localStorageHelper.setItem('FirstLaunchTimestamp', DateTime.now().toIso8601String());
+        localStorageHelper.setItem(
+            'FirstLaunchTimestamp', DateTime.now().toIso8601String());
       } else {
         debugPrint('This is not the first launch');
       }
 
       // Always update the last launch timestamp
-      localStorageHelper.setItem('LastLaunchTimestamp', DateTime.now().toIso8601String());
+      localStorageHelper.setItem(
+          'LastLaunchTimestamp', DateTime.now().toIso8601String());
 
-      final encryptedItem = await localStorageHelper.getEncryptedItem('SplashViewFirstEncrypted');
+      final encryptedItem =
+          await localStorageHelper.getEncryptedItem('SplashViewFirstEncrypted');
       debugPrint('Splash View Encrypted item: $encryptedItem');
-      
+
       // Navigate to home view
       await Future.delayed(Duration(seconds: 2));
       debugPrint('Navigating to home view');
@@ -48,12 +53,12 @@ class SplashView extends MasterView<SplashViewModel, SplashEvent, SplashState> {
       // Handle error
       debugPrint('Error occurred: $e');
     }
-  } 
+  }
+
   @override
   Widget viewContent(
       BuildContext context, SplashViewModel viewModel, SplashState state) {
     return Container(
-      color: Colors.white,
       padding: EdgeInsets.all(16.0),
       child: Center(
         child: CircularProgressIndicator(), // Only the splash animation
