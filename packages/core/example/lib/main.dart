3d502fe7bd8edfa91d1ce0c5b901a3967de84110
Nisa <76876477+nisacolak@users.noreply.github.com>
2025-07-19 02:58:28 +0300

    chore: streamline main function setup and enable development mode options

--- packages/core/example/lib/main.dart
+++ packages/core/example/lib/main.dart
@@ -4,19 +4,19 @@ import 'package:example/routes/app_routes.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
-  
   /// Initializes necessary components before the app starts.
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   // Perform any necessary setup before the app starts
-  await MasterApp.runBefore(
-    allowCollectDataTelemetry: true
-  );
+  await MasterApp.runBefore(allowCollectDataTelemetry: true);
   // Configure dependency injection for the application
   configureDependencies();
 
   // Run the main application with the specified router
   runApp(MasterApp(
+    devModeGrid: true,
+    devModeSpacer: true,
+
     router: appRouter, // The router handles navigation within the app
   ));
 }
