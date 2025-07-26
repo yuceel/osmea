4c0d05d0bde3509c0a6bdcf47925d4434cfa643c
Nisa <76876477+nisacolak@users.noreply.github.com>
2025-07-27 01:57:17 +0300

    feat: add initial slang configuration for localization

---
+++ projects/admin_dashboard/lib/resources/resources_en.g.dart
@@ -0,0 +1,65 @@
+///
+/// Generated file. Do not edit.
+///
+// coverage:ignore-file
+// ignore_for_file: type=lint, unused_import
+
+part of 'resources.g.dart';
+
+// Path: <root>
+typedef TranslationsEn = Translations; // ignore: unused_element
+class Translations implements BaseTranslations<AppLocale, Translations> {
+	/// Returns the current translations of the given [context].
+	///
+	/// Usage:
+	/// final resource = Translations.of(context);
+	static Translations of(BuildContext context) => InheritedLocaleData.of<AppLocale, Translations>(context).translations;
+
+	/// You can call this constructor and build your own translation instance of this locale.
+	/// Constructing via the enum [AppLocale.build] is preferred.
+	/// [AppLocaleUtils.buildWithOverrides] is recommended for overriding.
+	Translations({Map<String, Node>? overrides, PluralResolver? cardinalResolver, PluralResolver? ordinalResolver, TranslationMetadata<AppLocale, Translations>? meta})
+		: $meta = meta ?? TranslationMetadata(
+		    locale: AppLocale.en,
+		    overrides: overrides ?? {},
+		    cardinalResolver: cardinalResolver,
+		    ordinalResolver: ordinalResolver,
+		  ) {
+		$meta.setFlatMapFunction(_flatMapFunction);
+	}
+
+	/// Metadata for the translations of <en>.
+	@override final TranslationMetadata<AppLocale, Translations> $meta;
+
+	/// Access flat map
+	dynamic operator[](String key) => $meta.getTranslation(key);
+
+	late final Translations _root = this; // ignore: unused_field
+
+	Translations $copyWith({TranslationMetadata<AppLocale, Translations>? meta}) => Translations(meta: meta ?? this.$meta);
+
+	// Translations
+	late final TranslationsApplicationConfigEn application_config = TranslationsApplicationConfigEn._(_root);
+}
+
+// Path: application_config
+class TranslationsApplicationConfigEn {
+	TranslationsApplicationConfigEn._(this._root);
+
+	final Translations _root; // ignore: unused_field
+
+	// Translations
+	String get app_name => TranslationOverrides.string(_root.$meta, 'application_config.app_name', {}) ?? 'Admin Dashboard';
+}
+
+/// Flat map(s) containing all translations.
+/// Only for edge cases! For simple maps, use the map function of this library.
+extension on Translations {
+	dynamic _flatMapFunction(String path) {
+		switch (path) {
+			case 'application_config.app_name': return TranslationOverrides.string(_root.$meta, 'application_config.app_name', {}) ?? 'Admin Dashboard';
+			default: return null;
+		}
+	}
+}
+
