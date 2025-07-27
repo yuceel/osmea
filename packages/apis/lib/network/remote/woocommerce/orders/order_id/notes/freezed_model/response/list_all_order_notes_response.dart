54c7aa0b4b63d0e6217deba9cfcc09b904cea46c
melihalkbk <melihalkbk@gmail.com>
2025-07-27 03:29:32 +0300

    add: implement ListAllOrderNotesHandler and response model for WooCommerce API

---
+++ packages/apis/lib/network/remote/woocommerce/orders/order_id/notes/freezed_model/response/list_all_order_notes_response.dart
@@ -0,0 +1,73 @@
+// To parse this JSON data, do
+//
+//     final listAllOrderNotesResponse = listAllOrderNotesResponseFromJson(jsonString);
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'dart:convert';
+
+part 'list_all_order_notes_response.freezed.dart';
+part 'list_all_order_notes_response.g.dart';
+
+List<ListAllOrderNotesResponse> listAllOrderNotesResponseFromJson(String str) =>
+    List<ListAllOrderNotesResponse>.from(
+        json.decode(str).map((x) => ListAllOrderNotesResponse.fromJson(x)));
+
+String listAllOrderNotesResponseToJson(List<ListAllOrderNotesResponse> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+@freezed
+class ListAllOrderNotesResponse with _$ListAllOrderNotesResponse {
+  const factory ListAllOrderNotesResponse({
+    @JsonKey(name: "id") int? id,
+    @JsonKey(name: "author") String? author,
+    @JsonKey(name: "date_created") String? dateCreated,
+    @JsonKey(name: "date_created_gmt") String? dateCreatedGmt,
+    @JsonKey(name: "note") String? note,
+    @JsonKey(name: "customer_note") bool? customerNote,
+    @JsonKey(name: "_links") Links? links,
+  }) = _ListAllOrderNotesResponse;
+
+  factory ListAllOrderNotesResponse.fromJson(Map<String, dynamic> json) =>
+      _$ListAllOrderNotesResponseFromJson(json);
+}
+
+@freezed
+class Links with _$Links {
+  const factory Links({
+    @JsonKey(name: "self") List<Self>? self,
+    @JsonKey(name: "collection") List<Collection>? collection,
+    @JsonKey(name: "up") List<Collection>? up,
+  }) = _Links;
+
+  factory Links.fromJson(Map<String, dynamic> json) => _$LinksFromJson(json);
+}
+
+@freezed
+class Collection with _$Collection {
+  const factory Collection({
+    @JsonKey(name: "href") String? href,
+  }) = _Collection;
+
+  factory Collection.fromJson(Map<String, dynamic> json) =>
+      _$CollectionFromJson(json);
+}
+
+@freezed
+class Self with _$Self {
+  const factory Self({
+    @JsonKey(name: "href") String? href,
+    @JsonKey(name: "targetHints") TargetHints? targetHints,
+  }) = _Self;
+
+  factory Self.fromJson(Map<String, dynamic> json) => _$SelfFromJson(json);
+}
+
+@freezed
+class TargetHints with _$TargetHints {
+  const factory TargetHints({
+    @JsonKey(name: "allow") List<String>? allow,
+  }) = _TargetHints;
+
+  factory TargetHints.fromJson(Map<String, dynamic> json) =>
+      _$TargetHintsFromJson(json);
+}
