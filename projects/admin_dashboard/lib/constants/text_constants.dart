c0eb52bfc5c2f1cb676bc7788fb20eecefc2e91f
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2025-07-29 20:22:35 +0300

    feat: onboarding data structure for onboarding pages

---
+++ projects/admin_dashboard/lib/constants/text_constants.dart
@@ -0,0 +1,23 @@
+import 'package:admin_dashboard/gen/assets.gen.dart';
+import 'package:admin_dashboard/resources/resources.g.dart';
+
+final List<Map<String, String>> onboardingData = [
+      {
+        'title': resource.views.onboarding.page1.title,
+        'description':
+            resource.views.onboarding.page1.description,
+        'imagePath': Assets.images.onboardingPageOne.path,
+      },
+      {
+        'title': resource.views.onboarding.page2.title,
+        'description':
+            resource.views.onboarding.page2.description,
+        'imagePath': Assets.images.onboardingPageTwo.path,
+      },
+      {
+        'title': resource.views.onboarding.page3.title,
+        'description':
+            resource.views.onboarding.page3.description,
+        'imagePath': Assets.images.onboardingPageThree.path,
+      },
+    ];
