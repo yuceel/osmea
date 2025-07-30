caff6bf88644a63f718d120946c60ae25d543b27
Ayberk Görgün <ayberkgorgun@hotmail.com>
2025-07-31 00:45:38 +0300

    add: create InfoChipWidget for displaying configuration information in chip format

---
+++ packages/components/example_storybook/lib/storybook_test/components/searchbar_test_modular/widgets/info_chip_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+/// 💡 **Info Chip Widget - Shared Component**
+///
+/// Displays configuration information in chip format.
+class InfoChipWidget extends StatelessWidget {
+  final String label;
+  final String value;
+  final bool isDark;
+
+  const InfoChipWidget({
+    super.key,
+    required this.label,
+    required this.value,
+    this.isDark = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+      decoration: BoxDecoration(
+        color: isDark ? Colors.grey.shade800 : Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        border: Border.all(
+          color: isDark ? Colors.grey.shade700 : Colors.grey.shade300,
+        ),
+      ),
+      child: Text(
+        '$label: $value',
+        style: TextStyle(
+          fontSize: 12,
+          fontWeight: FontWeight.w500,
+          color: isDark ? Colors.white : Colors.black87,
+        ),
+      ),
+    );
+  }
+}
