dfcc014038af9fadee779b944e0c3e9085c12ede
Nisa <76876477+nisacolak@users.noreply.github.com>
2025-08-27 17:37:09 +0300

    feat: add dependency injection configuration for SplashViewModel using GetIt and Injectable

---
+++ projects/storefront_woo/lib/app/core/config/config_di.config.dart
@@ -0,0 +1,30 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:get_it/get_it.dart' as _i174;
+import 'package:injectable/injectable.dart' as _i526;
+import 'package:storefront_woo/app/views/view_splash/splash_view_model.dart'
+    as _i225;
+
+extension GetItInjectableX on _i174.GetIt {
+// initializes the registration of main-scope dependencies inside of GetIt
+  _i174.GetIt init({
+    String? environment,
+    _i526.EnvironmentFilter? environmentFilter,
+  }) {
+    final gh = _i526.GetItHelper(
+      this,
+      environment,
+      environmentFilter,
+    );
+    gh.factory<_i225.SplashViewModel>(() => _i225.SplashViewModel());
+    return this;
+  }
+}
