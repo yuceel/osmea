d1c6c7683290dbff2dade620d48c93839046e682
Nisa <76876477+nisacolak@users.noreply.github.com>
2025-09-03 18:32:30 +0300

    refactor: update import path for SplashViewModel and streamline GetIt initialization

--- projects/storefront_woo/lib/app/core/config/config_di.config.dart
+++ projects/storefront_woo/lib/app/core/config/config_di.config.dart
@@ -10,20 +10,16 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
-import 'package:storefront_woo/app/views/view_splash/splash_view_model.dart'
+import 'package:storefront_woo/app/views/view_splash/models/splash_view_model.dart'
     as _i225;
 
 extension GetItInjectableX on _i174.GetIt {
-// initializes the registration of main-scope dependencies inside of GetIt
+  // initializes the registration of main-scope dependencies inside of GetIt
   _i174.GetIt init({
     String? environment,
     _i526.EnvironmentFilter? environmentFilter,
   }) {
-    final gh = _i526.GetItHelper(
-      this,
-      environment,
-      environmentFilter,
-    );
+    final gh = _i526.GetItHelper(this, environment, environmentFilter);
     gh.factory<_i225.SplashViewModel>(() => _i225.SplashViewModel());
     return this;
   }
