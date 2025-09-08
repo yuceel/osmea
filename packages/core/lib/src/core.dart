84203c2643be6502585bc3527c20201684b4885c
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2025-09-08 18:49:34 +0300

    refactor: enhance Core class to initialize core dependencies using DI configuration

--- packages/core/lib/src/core.dart
+++ packages/core/lib/src/core.dart
@@ -1,10 +1,11 @@
 import 'package:get_it/get_it.dart'; // 📦 Importing the GetIt package for dependency injection
+import 'package:core/src/di/config/config_di.dart'; // 📦 Importing core DI configuration
 
 // 🏗️ Core class responsible for initializing core dependencies
 class Core {
   // 🚀 Initializes dependencies using GetIt instance
   Future<GetIt> init(GetIt getIt) async {
-    // ✅ Return the initialized GetIt instance
-    return getIt;
+    // ✅ Initialize core dependencies and return the configured GetIt instance
+    return await configureDependencies();
   }
 }
