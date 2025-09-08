b5a96cad0bcb6310cedfba2169c0a7c344358317
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2025-09-08 18:50:20 +0300

    refactor: update dependency injection configuration to use new SplashViewModel reference

--- projects/storefront_woo/lib/app/core/config/config_di.config.dart
+++ projects/storefront_woo/lib/app/core/config/config_di.config.dart
@@ -11,16 +11,20 @@
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
 import 'package:storefront_woo/app/views/view_splash/models/splash_view_model.dart'
-    as _i225;
+    as _i1070;
 
 extension GetItInjectableX on _i174.GetIt {
-  // initializes the registration of main-scope dependencies inside of GetIt
+// initializes the registration of main-scope dependencies inside of GetIt
   _i174.GetIt init({
     String? environment,
     _i526.EnvironmentFilter? environmentFilter,
   }) {
-    final gh = _i526.GetItHelper(this, environment, environmentFilter);
-    gh.factory<_i225.SplashViewModel>(() => _i225.SplashViewModel());
+    final gh = _i526.GetItHelper(
+      this,
+      environment,
+      environmentFilter,
+    );
+    gh.factory<_i1070.SplashViewModel>(() => _i1070.SplashViewModel());
     return this;
   }
 }
